3b503c794ec1dba6169ca3bb4abdbef4ad7ebbb7
Charlie Greenman <charlie@razroo.com>
2023-08-27 17:27:10 -0700

    Add code for angular-16.2.0 add-browser-animations-module

--- apps/test-codegen-eight/src/app/app.module.ts
+++ apps/test-codegen-eight/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
